3770caed6f64dd343dec45e4a7b40aeaae4e0e97
jsbalrog <edwardtjenkins@gmail.com>
2014-12-16 14:48:12 -0700

    Add oclazyload

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
-angular.module('solar', ['ui.router', 'gemini']).config(function($stateProvider) {
+angular.module('solar', ['ui.router', 'oc.lazyLoad']).config(function($stateProvider) {
   $stateProvider.state('site-survey', {
     templateUrl: 'scripts/gemini/site-survey.html',
-    controller: 'SiteSurveyCtrl as siteSurvey'
+    controller: 'SiteSurveyCtrl as siteSurvey',
+    resolve: {
+      gemini: function($ocLazyLoad) {
+        return $ocLazyLoad.load({
+          name: 'gemini',
+          files: ['scripts/gemini/module.js', 'scripts/gemini/SiteSurveyController.js']
+        });
+      }
+    }
   });
 });
